3b2d8dfa5420460cf2f5f5cfebbe13804081349f
enrique <king92salomon@gmail.com>
2024-04-10 15:44:32 +0800

    file

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://iammkrzgnupdfdfrqzta.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhbW1rcnpnbnVwZGZkZnJxenRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIzODkwNjIsImV4cCI6MjAyNzk2NTA2Mn0.vL6UCkJcZWfZB3hKzHDP8ipuz5TF91DKd8DTEHtAbbs";
+export const supabaseUrl = "";
+const supabaseKey = "";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
